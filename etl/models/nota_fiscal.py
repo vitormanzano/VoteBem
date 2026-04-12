ce6352429000044182e43e80953f8680347ebf0a
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-04-12 11:52:34 -0300

    fix: Arruamando models de nota fiscal e certidao criminal

--- etl/models/nota_fiscal.py
+++ etl/models/nota_fiscal.py
@@ -1,11 +1,14 @@
-from sqlalchemy import Column, BigInteger, String, ForeignKey, Numeric, Date, UniqueConstraint
+from sqlalchemy import Column, BigInteger, Integer, String, ForeignKey, Numeric, Date, UniqueConstraint
 from .base import Base
 
 class NotaFiscal(Base):
     __tablename__ = "nota_fiscal"
 
     id_nota = Column(BigInteger, primary_key=True, autoincrement=True)
-    sq_candidato = Column(BigInteger, ForeignKey("candidatura.sq_candidato"), nullable=False)
+    sq_candidato = Column(BigInteger, ForeignKey("candidatura.sq_candidato"), nullable=True)
+    cd_eleicao = Column(Integer, nullable=False)
+    nr_candidato = Column(Integer, nullable=False)
+    sg_uf = Column(String(2), nullable=False)
     nr_nota_fiscal = Column(String)
     nr_serie = Column(String)
     cpf_cnpj_emitente = Column(String)
@@ -15,5 +18,13 @@ class NotaFiscal(Base):
     nm_url_acesso = Column(String)
 
     __table_args__ = (
-        UniqueConstraint("sq_candidato", "nr_nota_fiscal", "cpf_cnpj_emitente"),
+        UniqueConstraint(
+            "cd_eleicao",
+            "nr_candidato",
+            "sg_uf",
+            "nr_nota_fiscal",
+            "cpf_cnpj_emitente",
+            name="uq_nota_fiscal",
+        ),
     )
+
