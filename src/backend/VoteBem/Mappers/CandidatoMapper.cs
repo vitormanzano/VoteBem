92a1233b9e962e26e276d890ff25051ef282051c
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2026-04-22 14:03:37 -0300

    refactor: Colocado letra maiuscula em cada palavra ao retornar o nome de um candidato

--- src/backend/VoteBem/Mappers/CandidatoMapper.cs
+++ src/backend/VoteBem/Mappers/CandidatoMapper.cs
@@ -1,15 +1,19 @@
-﻿using VoteBem.Dtos.Candidatos;
+﻿using System.Globalization;
+using VoteBem.Dtos.Candidatos;
 using VoteBem.Entities;
 
 namespace VoteBem.Mappers
 {
     public static class CandidatoMapper
     {
+        private static readonly TextInfo TextInfo = CultureInfo.GetCultureInfo("pt-BR").TextInfo;
+
         public static CandidatoPaginatedResponseDto MapCandidatoParaCandidatoPaginatedResponseDto(this Candidato candidato)
         {
             var ultimaCandidatura = candidato.Candidaturas.FirstOrDefault();
+            var nome = TextInfo.ToTitleCase((candidato.NmUrnaCandidato ?? candidato.NmCandidato).ToLower());
             return new CandidatoPaginatedResponseDto(
-                candidato.NmUrnaCandidato ?? candidato.NmCandidato,
+                nome,
                 ultimaCandidatura?.Partido?.SgPartido ?? "Dado não disponível",
                 ultimaCandidatura?.DsCargo ?? "Dado não disponível"
             );
